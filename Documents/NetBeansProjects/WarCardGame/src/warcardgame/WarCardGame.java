86b942de644dbf560a77cae585c3c0eced0bdb07
Ian <ianjblanchette@gmail.com>
2017-01-18 14:38:31 -0500

    Game of War Completed

--- Documents/NetBeansProjects/WarCardGame/src/warcardgame/WarCardGame.java
+++ Documents/NetBeansProjects/WarCardGame/src/warcardgame/WarCardGame.java
@@ -17,26 +17,8 @@ public class WarCardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Card myFirstCard = new Card("Ace", "Spades", 14);
-        System.out.printf("My card is: %s%n", myFirstCard);
-        
-        Card mySecondCard = new Card("Ace", "Hearts" , 14);
-        System.out.printf("My card is: %s%n", mySecondCard);
-        
-        DeckOfCards theDeck = new DeckOfCards();
-        System.out.printf("Our deck of cards includes: %n%s", theDeck.toString());
-        theDeck.shuffle();
-        
-        System.out.printf("%n%nAfter shuffling the deck it looks like: %n%s", theDeck.toString());
-        
-        ArrayList<Card> ianHand = new ArrayList<>();
-        
-        for (int i=1; i<= 5; i++)
-        {
-            ianHand.add(theDeck.dealTopCard());
-        }
-        
-        System.out.printf("%nIan's hand is %s%n", ianHand.toString());
+       gameOfWar theGame = new gameOfWar();
+       theGame.playTheGame();
     }
    
     
