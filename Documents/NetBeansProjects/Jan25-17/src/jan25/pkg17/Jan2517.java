eb97df47176d87449d83c749d50db789d5e384f3
Ian <ianjblanchette@gmail.com>
2017-01-25 13:26:18 -0500

    Creation Of PayCheque class

--- Documents/NetBeansProjects/Jan25-17/src/jan25/pkg17/Jan2517.java
+++ Documents/NetBeansProjects/Jan25-17/src/jan25/pkg17/Jan2517.java
@@ -5,6 +5,9 @@
  */
 package jan25.pkg17;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  *
  * @author Ian
@@ -15,7 +18,15 @@ public class Jan2517 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Employee employee1 = new Employee ("Fred","Flintstone", 1234, LocalDate.of(1998,Month.MARCH, 14));
+        
+        System.out.printf("Employee1 is %s%n", employee1);
+        System.out.printf("The employee was born on %s, that was a %s%n", employee1.getDateOfBirth(), employee1.getDateOfBirth().getDayOfWeek());
+        
+        Employee employee2 = new Employee ("Ian", "Blanchette", 4321, LocalDate.of(1990,Month.NOVEMBER, 15));
+        
+         System.out.printf("Employee2 is %s%n", employee2);
+        System.out.printf("The employee was born on %s, that was a %s%n", employee1.getDateOfBirth(), employee1.getDateOfBirth().getDayOfWeek());
     }
     
 }
